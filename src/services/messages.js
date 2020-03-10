f7905eeb9e550c1d1cbdaf8d141a1ade0b48cbaa
Alon Niv <oakfang@gmail.com>
2020-03-10 10:43:41 +0200

    remove coven

--- src/services/messages.js
+++ src/services/messages.js
@@ -1,36 +1,13 @@
-import React, {
-  createContext,
-  useContext,
-  useState,
-  useCallback,
-  useEffect,
-} from 'react';
-
-import { useCoven } from 'services/coven';
-import { username } from 'services/username';
-import { COVEN_BROKER } from 'config';
+import React, { createContext, useContext, useState, useCallback } from 'react';
 
 const MessageCtx = createContext();
 
 export function Provider({ children }) {
-  const coven = useCoven(COVEN_BROKER);
   const [messages, setMessages] = useState([]);
-  const push = useCallback(
+  const add = useCallback(
     message => setMessages(messages => [...messages, message]),
     [],
   );
-  const add = useCallback(
-    message => {
-      push(message);
-      coven.broadcast({ ...message, username });
-    },
-    [push, coven],
-  );
-  useEffect(() => {
-    const handler = ({ message }) => push(message);
-    coven.on('message', handler);
-    return () => coven.off('message', handler);
-  }, [push, coven]);
   const ctx = { messages, add };
   return <MessageCtx.Provider value={ctx}>{children}</MessageCtx.Provider>;
 }
